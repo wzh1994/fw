d2ee7e3c146666ef8d3560cf1cea22d2fb8c4c0f
wangzhihong <wangzhihong@sensetime.com>
2019-05-07 19:04:23 +0800

    show pointtoline code

--- fw/play.cpp
+++ fw/play.cpp
@@ -7,7 +7,13 @@
 #include <thread>
 #include "firework.h"
 #include <opencv2/opencv.hpp>
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
 #include <vector>
+#include "kernels.h"
+#include "utils.h"
+#include "shader.h"
+#include "timer.h"
 
 // constant global variables
 namespace {
@@ -27,8 +33,7 @@ void pushImg(std::vector<cv::Mat>& imgs) {
 	imgs.push_back(img);
 }
 
-
-void init() {
+void init(size_t w = screenWidth, size_t h = screenHeight) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -36,7 +41,7 @@ void init() {
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	glfwWindowHint(GLFW_SAMPLES, 8);
 
-	window = glfwCreateWindow(screenWidth, screenHeight, "Firework", nullptr, nullptr);
+	window = glfwCreateWindow(w, h, "Firework", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Options
@@ -58,14 +63,24 @@ void playInNewThread(firework::FireWorkType type,
 	Camera c(screenWidth, screenHeight);
 	int i = 0;
 	std::vector<cv::Mat> imgs;
+	// glPolygonMode(GL_FRONT, GL_LINE);
+	Timer timer1;
+	Timer timer2;
+	AverageTime averageTime1(49);
+	AverageTime averageTime2(49);
 	while (!glfwWindowShouldClose(window)) {
+		timer1.start();
+		timer2.start();
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 		glClearColor(0, 0, 0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_MULTISAMPLE);
 		fw->GetParticles(i++);
+		averageTime1.append(timer1.stop());
 		fw->RenderScene(c);
+		averageTime2.append(timer2.stop());
+		cout << averageTime1.averageTime() << " " << averageTime2.averageTime() << endl;
 		if (genVideo) {
 			pushImg(imgs);
 		}
@@ -76,6 +91,7 @@ void playInNewThread(firework::FireWorkType type,
 				i = 0;
 		}
 	}
+
 	if (genVideo) {
 		//CV_FOURCC('M', 'J', 'P', 'G');
 		cv::VideoWriter writer(fname + ".avi", cv::CAP_ANY, 24.0, cv::Size(400, 400));
@@ -96,11 +112,97 @@ void playAndSave(firework::FireWorkType type, float* args, const char* str, size
 	playInNewThread(type, args, true, fname);
 }
 
+void showPointToLine() {
+	init(1200, 1200);
+	GLuint vbo=0, ebo=0, vao=0;
+	size_t vboSize = 1000000, eboSize = 1000000;
+	struct cudaGraphicsResource *cuda_vbo_resource_, *cuda_ebo_resource_;
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER,
+		vboSize * sizeof(float), nullptr, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	CUDACHECK(cudaGraphicsGLRegisterBuffer(&cuda_vbo_resource_, vbo,
+		cudaGraphicsMapFlagsWriteDiscard));
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		eboSize * sizeof(int), nullptr, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	CUDACHECK(cudaGraphicsGLRegisterBuffer(&cuda_ebo_resource_, ebo,
+		cudaGraphicsMapFlagsWriteDiscard));
+
+	CUDACHECK(cudaDeviceSynchronize());
+
+	void *pVboData, *pEboData;
+	size_t sizeVbo, sizeEbo;
+	CUDACHECK(cudaGraphicsMapResources(1, &cuda_vbo_resource_, 0));
+	CUDACHECK(cudaGraphicsMapResources(1, &cuda_ebo_resource_, 0));
+	CUDACHECK(cudaGraphicsResourceGetMappedPointer(
+		&pVboData, &sizeVbo, cuda_vbo_resource_));
+	CUDACHECK(cudaGraphicsResourceGetMappedPointer(
+		&pEboData, &sizeEbo, cuda_ebo_resource_));
+	CUDACHECK(cudaDeviceSynchronize());
+
+	float points[15]{-0.3, 0, 0, -0.15, 0, 0, 0, 0.03, 0, 0.15, 0.09, 0, 0.3, 0.06, 0.06};
+	float colors[15]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0 };
+	float sizes[5]{ 0.2, 0.1, 0.08, 0.12, 0.16};
+	size_t offsets[2]{ 0, 5 };
+	float *dPoints, *dColors, *dSizes;
+	size_t *dOffsets;
+	cudaKernel::cudaMallocAndCopy(dPoints, points, 15);
+	cudaKernel::cudaMallocAndCopy(dColors, colors, 15);
+	cudaKernel::cudaMallocAndCopy(dSizes, sizes, 5);
+	cudaKernel::cudaMallocAndCopy(dOffsets, offsets, 2);
+	size_t newEboSize = cudaKernel::pointToLine(dPoints, dSizes, dColors,
+		5, dOffsets, 1, static_cast<float*>(pVboData),
+		static_cast<uint32_t*>(pEboData), 1, 0, 0);
+
+	cudaGraphicsUnmapResources(1, &cuda_vbo_resource_, 0);
+	cudaGraphicsUnmapResources(1, &cuda_ebo_resource_, 0);
+
+	glDeleteVertexArrays(1, &vao);
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+		7 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE,
+		7 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	Camera camera(1200, 1200);
+	Shader shader("fw.vs", "fw.fs");
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+		glClearColor(1, 1, 1, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_MULTISAMPLE);
+		shader.use();
+		shader.setMat4("view", camera.GetViewMatrix());
+		shader.setMat4("projection", camera.GetProjectionMatrix());
+		glBindVertexArray(vao);
+		// draw points 0-3 from the currently bound VAO with current in-use shader;
+		//glEnable(GL_BLEND);
+		//glBlendFunc(GL_SRC_ALPHA, GL_DST_ALPHA);
+		//glBlendEquation(GL_MAX);
+		glDepthMask(GL_TRUE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawElements(GL_TRIANGLES, newEboSize, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
+	glfwTerminate();
+}
+
 int main() {
-	float* args = new float[1000]{
-		0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.5098039507865906, 0.250980406999588, 0.16862745583057404, 0.5529412031173706, 0.3490196168422699, 0.2823529541492462, 0.572549045085907, 0.250980406999588, 0.0941176488995552, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 0.8799999952316284, 0.9300000071525574, 0.0, 10.0, 0.0, 35.0, 0.15000000596046448, 28.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.800000011920929, 0.7599999904632568, 0.7200000286102295, 0.6800000071525574, 0.6399999856948853, 0.6000000238418579, 0.5600000023841858, 0.5199999809265137, 0.47999998927116394, 0.4399999976158142, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.4000000059604645, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, 70.0, 70.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 0.800000011920929, 0.0, 0.0, 10.0, 0.0, 35.0, 0.699999988079071, 50.0
-	};
-	std::thread t(playInNewThread, firework::FireWorkType::Mixture, args, false, "");
-	delete[] args;
+	std::thread t(showPointToLine);
 	t.join();
 }
